69e69703f7882b5bcb30fb8852128f96bd6160ba
Sam.Xu <Sam.Xu@advantech.com>
2025-02-07 11:33:33 +0800

    IotDb connection configurations.
    And IotDbInsertRepository.

---
+++ Data Management Service/test/IotDb.MeasurementManagement.IotDb.Tests/MeasurementManagementIotDbTestModule.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.Modularity;
+
+namespace IotDb.MeasurementManagement;
+
+[DependsOn(
+    typeof(MeasurementManagementIobDbModule),
+    typeof(MeasurementManagementTestBaseModule)
+)]
+public class MeasurementManagementIobDbModule : AbpModule
+{
+
+}
