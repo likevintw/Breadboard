3e7acbfa7150806b6e44b0d0644664f7c1768caa
Sam.Xu <Sam.Xu@advantech.com>
2025-02-10 11:15:25 +0800

    Validator.

---
+++ Data Management Service/src/IotDb.MeasurementManagement.Application/Validator/GetCpuPageByTimeRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using IotDb.MeasurementManagement.Cpu;
+
+namespace IotDb.MeasurementManagement.Validator
+{
+    public class GetCpuPageByTimeRequestValidator: AbstractValidator<GetCpuPageByTimeRequest>
+    {
+        public GetCpuPageByTimeRequestValidator() {
+            RuleFor(x => x.StartDateTime)
+                .GreaterThan(x => x.EndDateTime)
+                .WithMessage("Start date time can't be greater than end date time.");
+            RuleFor(x => x.Page.SkipCount)
+                .NotEmpty().WithMessage("Skip count can't be null/empty.");
+        }
+    }
+}
