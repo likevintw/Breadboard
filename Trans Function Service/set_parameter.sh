80d9173a90d4fb2e7f9e4f5483eaf8b6cb65dd07
kengwei.li <likevintw@gmail.com>
2025-02-12 11:49:23 +0800

    update nats environments setting

--- Trans Function Service/set_parameter.sh
+++ Trans Function Service/set_parameter.sh
@@ -12,3 +12,10 @@ echo $IOTDB_URL &&\
 echo $IOTDB_PORT &&\
 echo $IOTDB_USERNAME &&\
 echo $IOTDB_PASSWORD
+
+export NATS_URL="172.22.23.75:2222" &&\
+export NATS_USERNAME="wils8wrh" &&\
+export NATS_PASSWORD="nsdjmckwn" &&\
+echo $NATS_URL &&\
+echo $NATS_USERNAME &&\
+echo $NATS_PASSWORD
